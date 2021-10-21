5e40821ca9827d5d1e6bf8a0d7b708869513dfb5
KatsuroDev <katsuro.userg@gmail.com>
2021-10-21 14:35:32 -0400

    add complete index page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TP2_Serveur
+1929736
+Alexis Millette
 TP Ajax en Service d'échange de données
---
+++ js/index.js
@@ -0,0 +1,40 @@
+const SERVICE_URL = "https://api.andromia.science/monsters/atlas";
+
+$(document).ready(() => {
+    getMonsters();
+});
+
+async function getMonsters() {
+    try {
+        const response = await axios.get(SERVICE_URL);
+        if(response.status === 200) {
+            const monsters = response.data;
+            monsters.forEach(m => {
+                $('#monsters').append(displayMonsters(m));
+            });
+        } else {
+            console.log(response);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+function displayMonsters(monster) {
+    let monsterHtml = '<tr>';
+
+    monsterHtml += `<th scope="row">${monster.atlasNumber}<img class="monsterpic" src="${monster.assets}"></th>`;
+    monsterHtml += `<td><a href="details.html?atlasnumber=${monster.atlasNumber}">${monster.name}</a></td>`;
+    monsterHtml += `<td>[${monster.health.min}, ${monster.health.max}]</td>`;
+    monsterHtml += `<td>[${monster.damage.min}, ${monster.damage.max}]</td>`;
+    monsterHtml += `<td>[${monster.speed.min}, ${monster.speed.max}]</td>`;
+    monsterHtml += `<td>[${percentage(monster.critical.min)}, ${percentage(monster.critical.max)}]%</td>`;
+
+    monsterHtml += '</tr>';
+    return monsterHtml;
+}
+
+function percentage(number) {
+    const result = number*100;
+    return result.toFixed(2);
+}
